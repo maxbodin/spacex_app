816778565507ae34ad383fe1dce99937e5978e85
Maxime Bodin <maxime.bodin@mailo.com>
2025-11-14 22:11:50 +0100

    Improving launch model to get more data.

--- lib/data/models/launch.model.dart
+++ lib/data/models/launch.model.dart
@@ -2,13 +2,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'launch.model.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class LaunchModel {
   @JsonKey(name: 'id')
   final String id;
 
   @JsonKey(name: 'name')
   final String name;
+
+  @JsonKey(name: 'flight_number')
+  final int flightNumber;
 
   @JsonKey(name: 'details')
   final String? details;
@@ -28,15 +31,41 @@ class LaunchModel {
   @JsonKey(name: 'rocket')
   final String rocketId;
 
+  @JsonKey(name: 'crew')
+  final List<String> crew;
+
+  @JsonKey(name: 'ships')
+  final List<String> ships;
+
+  @JsonKey(name: 'capsules')
+  final List<String> capsules;
+
+  @JsonKey(name: 'payloads')
+  final List<String> payloads;
+
+  @JsonKey(name: 'launchpad')
+  final String launchpad;
+
+  @JsonKey(name: 'cores')
+  final List<CoreModel> cores;
+
+
   LaunchModel({
     required this.id,
     required this.name,
+    required this.flightNumber,
     this.details,
     required this.dateUtc,
     this.success,
     required this.links,
     required this.failures,
     required this.rocketId,
+    required this.crew,
+    required this.ships,
+    required this.capsules,
+    required this.payloads,
+    required this.launchpad,
+    required this.cores,
   });
 
   factory LaunchModel.fromJson(Map<String, dynamic> json) =>
@@ -45,15 +74,34 @@ class LaunchModel {
   Map<String, dynamic> toJson() => _$LaunchModelToJson(this);
 }
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class LinksModel {
   @JsonKey(name: 'patch')
   final PatchModel patch;
 
+  @JsonKey(name: 'reddit')
+  final RedditModel reddit;
+
+  @JsonKey(name: 'flickr')
+  final FlickrModel flickr;
+
+  @JsonKey(name: 'webcast')
+  final String? webcast;
+
   @JsonKey(name: 'article')
   final String? article;
 
-  LinksModel({required this.patch, this.article});
+  @JsonKey(name: 'wikipedia')
+  final String? wikipedia;
+
+  LinksModel({
+    required this.patch,
+    required this.reddit,
+    required this.flickr,
+    this.webcast,
+    this.article,
+    this.wikipedia,
+  });
 
   factory LinksModel.fromJson(Map<String, dynamic> json) =>
       _$LinksModelFromJson(json);
@@ -78,6 +126,45 @@ class PatchModel {
 }
 
 @JsonSerializable()
+class RedditModel {
+  @JsonKey(name: 'campaign')
+  final String? campaign;
+
+  @JsonKey(name: 'launch')
+  final String? launch;
+
+  @JsonKey(name: 'media')
+  final String? media;
+
+  @JsonKey(name: 'recovery')
+  final String? recovery;
+
+  RedditModel({this.campaign, this.launch, this.media, this.recovery});
+
+  factory RedditModel.fromJson(Map<String, dynamic> json) =>
+      _$RedditModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RedditModelToJson(this);
+}
+
+@JsonSerializable()
+class FlickrModel {
+  @JsonKey(name: 'small')
+  final List<String> small;
+
+  @JsonKey(name: 'original')
+  final List<String> original;
+
+  FlickrModel({required this.small, required this.original});
+
+  factory FlickrModel.fromJson(Map<String, dynamic> json) =>
+      _$FlickrModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FlickrModelToJson(this);
+}
+
+
+@JsonSerializable()
 class FailureModel {
   @JsonKey(name: 'reason')
   final String reason;
@@ -88,4 +175,51 @@ class FailureModel {
       _$FailureModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$FailureModelToJson(this);
+}
+
+@JsonSerializable()
+class CoreModel {
+  @JsonKey(name: 'core')
+  final String? core;
+
+  @JsonKey(name: 'flight')
+  final int? flight;
+
+  @JsonKey(name: 'gridfins')
+  final bool? gridfins;
+
+  @JsonKey(name: 'legs')
+  final bool? legs;
+
+  @JsonKey(name: 'reused')
+  final bool? reused;
+
+  @JsonKey(name: 'landing_attempt')
+  final bool? landingAttempt;
+
+  @JsonKey(name: 'landing_success')
+  final bool? landingSuccess;
+
+  @JsonKey(name: 'landing_type')
+  final String? landingType;
+
+  @JsonKey(name: 'landpad')
+  final String? landpad;
+
+  CoreModel({
+    this.core,
+    this.flight,
+    this.gridfins,
+    this.legs,
+    this.reused,
+    this.landingAttempt,
+    this.landingSuccess,
+    this.landingType,
+    this.landpad,
+  });
+
+  factory CoreModel.fromJson(Map<String, dynamic> json) =>
+      _$CoreModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CoreModelToJson(this);
 }
--- lib/data/models/launch.model.g.dart
+++ lib/data/models/launch.model.g.dart
@@ -9,6 +9,7 @@ part of 'launch.model.dart';
 LaunchModel _$LaunchModelFromJson(Map<String, dynamic> json) => LaunchModel(
   id: json['id'] as String,
   name: json['name'] as String,
+  flightNumber: (json['flight_number'] as num).toInt(),
   details: json['details'] as String?,
   dateUtc: DateTime.parse(json['date_utc'] as String),
   success: json['success'] as bool?,
@@ -17,27 +18,57 @@ LaunchModel _$LaunchModelFromJson(Map<String, dynamic> json) => LaunchModel(
       .map((e) => FailureModel.fromJson(e as Map<String, dynamic>))
       .toList(),
   rocketId: json['rocket'] as String,
+  crew: (json['crew'] as List<dynamic>).map((e) => e as String).toList(),
+  ships: (json['ships'] as List<dynamic>).map((e) => e as String).toList(),
+  capsules: (json['capsules'] as List<dynamic>)
+      .map((e) => e as String)
+      .toList(),
+  payloads: (json['payloads'] as List<dynamic>)
+      .map((e) => e as String)
+      .toList(),
+  launchpad: json['launchpad'] as String,
+  cores: (json['cores'] as List<dynamic>)
+      .map((e) => CoreModel.fromJson(e as Map<String, dynamic>))
+      .toList(),
 );
 
 Map<String, dynamic> _$LaunchModelToJson(LaunchModel instance) =>
     <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
+      'flight_number': instance.flightNumber,
       'details': instance.details,
       'date_utc': instance.dateUtc.toIso8601String(),
       'success': instance.success,
-      'links': instance.links,
-      'failures': instance.failures,
+      'links': instance.links.toJson(),
+      'failures': instance.failures.map((e) => e.toJson()).toList(),
       'rocket': instance.rocketId,
+      'crew': instance.crew,
+      'ships': instance.ships,
+      'capsules': instance.capsules,
+      'payloads': instance.payloads,
+      'launchpad': instance.launchpad,
+      'cores': instance.cores.map((e) => e.toJson()).toList(),
     };
 
 LinksModel _$LinksModelFromJson(Map<String, dynamic> json) => LinksModel(
   patch: PatchModel.fromJson(json['patch'] as Map<String, dynamic>),
+  reddit: RedditModel.fromJson(json['reddit'] as Map<String, dynamic>),
+  flickr: FlickrModel.fromJson(json['flickr'] as Map<String, dynamic>),
+  webcast: json['webcast'] as String?,
   article: json['article'] as String?,
+  wikipedia: json['wikipedia'] as String?,
 );
 
 Map<String, dynamic> _$LinksModelToJson(LinksModel instance) =>
-    <String, dynamic>{'patch': instance.patch, 'article': instance.article};
+    <String, dynamic>{
+      'patch': instance.patch.toJson(),
+      'reddit': instance.reddit.toJson(),
+      'flickr': instance.flickr.toJson(),
+      'webcast': instance.webcast,
+      'article': instance.article,
+      'wikipedia': instance.wikipedia,
+    };
 
 PatchModel _$PatchModelFromJson(Map<String, dynamic> json) => PatchModel(
   small: json['small'] as String?,
@@ -47,8 +78,57 @@ PatchModel _$PatchModelFromJson(Map<String, dynamic> json) => PatchModel(
 Map<String, dynamic> _$PatchModelToJson(PatchModel instance) =>
     <String, dynamic>{'small': instance.small, 'large': instance.large};
 
+RedditModel _$RedditModelFromJson(Map<String, dynamic> json) => RedditModel(
+  campaign: json['campaign'] as String?,
+  launch: json['launch'] as String?,
+  media: json['media'] as String?,
+  recovery: json['recovery'] as String?,
+);
+
+Map<String, dynamic> _$RedditModelToJson(RedditModel instance) =>
+    <String, dynamic>{
+      'campaign': instance.campaign,
+      'launch': instance.launch,
+      'media': instance.media,
+      'recovery': instance.recovery,
+    };
+
+FlickrModel _$FlickrModelFromJson(Map<String, dynamic> json) => FlickrModel(
+  small: (json['small'] as List<dynamic>).map((e) => e as String).toList(),
+  original: (json['original'] as List<dynamic>)
+      .map((e) => e as String)
+      .toList(),
+);
+
+Map<String, dynamic> _$FlickrModelToJson(FlickrModel instance) =>
+    <String, dynamic>{'small': instance.small, 'original': instance.original};
+
 FailureModel _$FailureModelFromJson(Map<String, dynamic> json) =>
     FailureModel(reason: json['reason'] as String);
 
 Map<String, dynamic> _$FailureModelToJson(FailureModel instance) =>
     <String, dynamic>{'reason': instance.reason};
+
+CoreModel _$CoreModelFromJson(Map<String, dynamic> json) => CoreModel(
+  core: json['core'] as String?,
+  flight: (json['flight'] as num?)?.toInt(),
+  gridfins: json['gridfins'] as bool?,
+  legs: json['legs'] as bool?,
+  reused: json['reused'] as bool?,
+  landingAttempt: json['landing_attempt'] as bool?,
+  landingSuccess: json['landing_success'] as bool?,
+  landingType: json['landing_type'] as String?,
+  landpad: json['landpad'] as String?,
+);
+
+Map<String, dynamic> _$CoreModelToJson(CoreModel instance) => <String, dynamic>{
+  'core': instance.core,
+  'flight': instance.flight,
+  'gridfins': instance.gridfins,
+  'legs': instance.legs,
+  'reused': instance.reused,
+  'landing_attempt': instance.landingAttempt,
+  'landing_success': instance.landingSuccess,
+  'landing_type': instance.landingType,
+  'landpad': instance.landpad,
+};
